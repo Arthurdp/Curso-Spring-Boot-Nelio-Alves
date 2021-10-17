27b456eb736e9b2f43427fb0d1d9776109e3a70d
Arthur Dantas <arthur.porto@ccc.ufcg.edu.br>
2021-10-17 16:24:48 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ Aulas Exercícios/cursomc/src/main/java/com/cursospring/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.cursospring.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursospring.cursomc.domain.Pedido;
+import com.cursospring.cursomc.exceptions.ObjectNotFoundException;
+import com.cursospring.cursomc.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
+				+ ", Tipo: " + Pedido.class.getName()));
+	}
+}
